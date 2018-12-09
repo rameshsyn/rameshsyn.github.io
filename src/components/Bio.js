1425e564a1b914008d7802c3a51fc9c50d7001c6
Ramesh Syangtan <rameshsyangtan92@gmail.com>
2018-12-09 17:25:19 +0545

    Initial commit

---
+++ src/components/Bio.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Ramesh Syangtan`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+            borderRadius: '50%',
+          }}
+        />
+        <p>
+          A personal blog by{' '}
+          <a href="https://twitter.com/ramesh_syn">
+            <strong>Ramesh Syangtan</strong>
+          </a>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
---
+++ src/pages/hello-world.md
@@ -0,0 +1,6 @@
+---
+title: Hello World
+date: '2018-12-04'
+---
+
+Hello world. This is a fake blog post.
