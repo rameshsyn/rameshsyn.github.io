2097cf4e05be831ec1238d08e8f4cad66efdce6e
Ramesh Syangtan <rameshsyangtan92@gmail.com>
2021-02-28 11:28:49 +0545

    sonetel

---
+++ src/pages/test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class BlogIndex extends React.Component {
+  render() {
+    return (
+      <div>
+        <script
+          async
+          id="slcLiveChat"
+          src="https://widget.sonetel.com/SonetelWidget.min.js"
+          data-account-id="207608483"
+        ></script>
+      </div>
+    )
+  }
+}
+
+export default BlogIndex
